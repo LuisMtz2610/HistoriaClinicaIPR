c8cce349a78828ea6a328cf0c4b3b0a0b035087e
LuisMtz2610 <luis.martinez@martineziturriaga.com>
2025-12-15 10:35:36 -0600

    Version 1.1.2

---
+++ types/html2canvas.d.ts
@@ -0,0 +1,7 @@
+// Auto-added to prevent Vercel/tsc from failing when html2canvas is referenced in optional/unused files.
+// If you actually use html2canvas at runtime, install it: npm i html2canvas
+
+declare module 'html2canvas' {
+  const html2canvas: any
+  export default html2canvas
+}
